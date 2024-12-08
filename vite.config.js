98c3e85f021503efeca826ea1e331c3f1fe23fb1
BryanDiaz-dev <k4013165@gmail.com>
2024-12-08 12:44:07 -0300

    Actualizar configuración para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,30 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/BryanDiaz-dev.github.io/',
+  base: './',
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   },
   build: {
+    outDir: 'docs',
     rollupOptions: {
       output: {
-        format: 'es'
+        format: 'es',
+        assetFileNames: 'assets/[name].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Content-Type': 'application/javascript'
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
       }
     }
   }
