742f5f158a373e6d5bddbf7d76a794c1ecc125ba
BRYAN DANIEL DÍAZ GUERRA <56714225@ceibal.edu.uy>
2024-12-08 12:30:43 -0300

    Fix: MIME type configuration

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bryandiaz-dev.github.io/',
+  base: '/BryanDiaz-dev.github.io/',
   server: {
     host: '0.0.0.0',    // Permite acceso desde cualquier IP
     port: 80,           // Puerto 80
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
   }
 })
