3f6ebe9ed4295fa64412b85309c32ab9ac571686
Bryan Diaz <k4013165@gmail.com>
2024-12-08 12:29:16 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/BryanDiaz-dev.github.io/',
   plugins: [react()],
-  base: '/bryandiaz-dev.github.io/',
-  server: {
-    host: '0.0.0.0',    // Permite acceso desde cualquier IP
-    port: 80,           // Puerto 80
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
   }
 })
