43faf2238c5a01b0f1b3826af425adb473c847ad
BryanDiaz-dev <k4013165@gmail.com>
2024-12-11 00:28:58 -0300

    arreglo ruta

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,7 +5,7 @@ export const projects = [
     title: "Blog Personal",
     route: "bryandiaz-dev.duckdns.org/blogPersonal/", 
     description: "Blog con sistema de gestión de contenidos y comentarios",
-    image: "../assets/images/blogPersonal.png",
+    image: "../assets/images/proyectos/blogPersonal.jpeg",
   },
   {
     id: 2,
